23710fe7ed40c773cbb7ecf2e9d270e9156a8690
Karl Dubost <karl+github@la-grange.net>
2014-07-15 15:32:19 +0900

    Shell for converting mozilla bugzilla data

---
+++ importer/adapters/mozilla_adapter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+def convert_json_data(json_data):
+    '''Take the json data from the bug repo and convert it
+    into json data suitable for webcompat.'''
+    pass
